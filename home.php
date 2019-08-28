b0546e7a05ef49b40c5b22bfe9c5865dd5dd1aa2
Petrisor Neagu <petrisor@madball.ro>
2019-08-28 19:03:01 +0300

    user id in session

--- home.php
+++ home.php
@@ -1,8 +1,8 @@
 <?php
 include 'core/init.php';
 
-//$user_id = $_SESSION['user_id'];
-//$user = $userObj->userData($user_id);
+$user_id = $_SESSION['user_id'];
+$user = $userObj->userData($user_id);
 // echo $user_id;
 
 ?>
